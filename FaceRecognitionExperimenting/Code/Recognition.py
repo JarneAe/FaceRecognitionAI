9ac31101bcaef390f5e3df44d252364d9c0eb15c
JarneAe <jarnea408@gmail.com>
2021-02-08 23:07:23 +0100

    Made code work with new file structure

--- FaceRecognitionExperimenting/Code/Recognition.py
+++ FaceRecognitionExperimenting/Code/Recognition.py
@@ -8,25 +8,25 @@ name = ''
 def Recognition():
     global name
     global pil_image
-    obama_image = face_recognition.load_image_file("Obama.jpg")
+    obama_image = face_recognition.load_image_file("Images\Obama.jpg")
     obama_face_encodings = face_recognition.face_encodings(obama_image) [0]
 
-    robertdowney_image = face_recognition.load_image_file("robertdowney.jpg")
+    robertdowney_image = face_recognition.load_image_file("Images\\robertdowney.jpg")
     robertdowney_face_encodings = face_recognition.face_encodings(robertdowney_image) [0]
 
-    favreau_image = face_recognition.load_image_file("favreau.jpg")
+    favreau_image = face_recognition.load_image_file("Images\\favreau.jpg")
     favreau_face_encodings = face_recognition.face_encodings(favreau_image) [0]
 
-    Mickey_image = face_recognition.load_image_file("Mickey.jpg")
+    Mickey_image = face_recognition.load_image_file("Images\Mickey.jpg")
     Mickey_face_encodings = face_recognition.face_encodings(Mickey_image) [0]
 
-    Scarlett_image = face_recognition.load_image_file("Scarlett.jpg")
+    Scarlett_image = face_recognition.load_image_file("Images\Scarlett.jpg")
     Scarlett_face_encodings = face_recognition.face_encodings(Scarlett_image) [0]
 
-    Gwyneth_image = face_recognition.load_image_file("Gwyneth.jpg")
+    Gwyneth_image = face_recognition.load_image_file("Images\Gwyneth.jpg")
     Gwyneth_face_encodings = face_recognition.face_encodings(Gwyneth_image) [0]
 
-    Don_image = face_recognition.load_image_file("Don.jpg")
+    Don_image = face_recognition.load_image_file("Images\Don.jpg")
     Don_face_encodings = face_recognition.face_encodings(Don_image) [0]
 
     known_face_encodings = [
@@ -53,7 +53,7 @@ def Recognition():
 
 
 
-    image = face_recognition.load_image_file("2obamas.jpg")
+    image = face_recognition.load_image_file("Images\\2obamas.jpg")
 
     face_locations = face_recognition.face_locations(image)
     face_encodings = face_recognition.face_encodings(image, face_locations)
@@ -72,7 +72,7 @@ def Recognition():
             name = known_face_names[first_match_index]
 
         draw.rectangle(((left,top),(right,bottom)),outline=(0,0,255),width=(2))
-        font = ImageFont.truetype("LEMONMILK-Regular.otf",12)
+        font = ImageFont.truetype("Fonts\LEMONMILK-Regular.otf",12)
         text_width,text_height = draw.textsize(name)
         draw.rectangle(((left,bottom - text_height - 10),(right,bottom)), fill=(0,0,255),outline=(0,0,255))
         draw.text((left + 6, bottom - text_height -5),name,font=font, fill=(255,255,255,255))
